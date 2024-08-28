f0f5badf594ba1b13bf95bce23306ada33ecca6b
Isa-brito <britoisabela87@gmail.com>
2024-08-28 12:35:01 +0000

    add: código teste de idade

---
+++ TesteIdade.java
@@ -0,0 +1,21 @@
+class TesteIdade {
+    public  static void main(String[] args) {
+        int idade;
+        idade = 21;
+
+        int  idadeNoAnoQueVem = idade + 1;
+        double resultado = idade / 2;
+
+        boolean maiorIdade;
+        maiorIdade  = idade >= 18;
+
+        boolean tipoSangue = 'a' == 'a';
+
+        int i = 5;
+        int j = i;
+        i = i +1;
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
